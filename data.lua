bbdaab8dc1dbe3e33ec5aaeb89f3d03ce053d117
Axel Örn Sigurðsson <axel@absalon.is>
2019-06-18 21:34:33 +0200

    Simplify structure

--- data.lua
+++ data.lua
@@ -1,1 +1,38 @@
-require("prototypes.sounds")
+data:extend({
+  {
+    type = "sound",
+    name = "geiger-close",
+    filename = "__uranium_geiger__/sounds/geiger-close.ogg",
+    volume = 0.7,
+    aggregation = {
+      max_count = 1,
+      remove = true,
+      count_already_playing = true,
+      progress_threshold = 3
+    }
+  },
+  {
+    type = "sound",
+    name = "geiger-middle",
+    filename = "__uranium_geiger__/sounds/geiger-middle.ogg",
+    volume = 0.6,
+    aggregation = {
+      max_count = 1,
+      remove = true,
+      count_already_playing = true,
+      progress_threshold = 3
+    }
+  },
+  {
+    type = "sound",
+    name = "geiger-distant",
+    filename = "__uranium_geiger__/sounds/geiger-distant.ogg",
+    volume = 0.5,
+    aggregation = {
+      max_count = 1,
+      remove = true,
+      count_already_playing = true,
+      progress_threshold = 3
+    }
+  },
+})
