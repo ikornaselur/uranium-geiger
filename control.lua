f466614c8ce695afd5f2eb1b007b803a8b6f94df
Axel Örn Sigurðsson <axel@absalon.is>
2019-06-21 19:29:51 +0200

    Lint

--- control.lua
+++ control.lua
@@ -1,36 +1,37 @@
 local function withinRadius(player, radius)
-  local surface = player.surface
-  local position = player.position
-  local entities = surface.find_entities_filtered{
-    position=position,
-    radius=radius,
-    limit=1,
-    name="uranium-ore"
-  }
+    local surface = player.surface
+    local position = player.position
+    local entities =
+        surface.find_entities_filtered {
+        position = position,
+        radius = radius,
+        limit = 1,
+        name = "uranium-ore"
+    }
 
-  return next(entities) ~= nil;
+    return next(entities) ~= nil
 end
 
-script.on_event({defines.events.on_tick},
-function (e)
-  if e.tick % 60 == 0 then
-    local player = game.players[1]
+script.on_event(
+    {defines.events.on_tick},
+    function(e)
+        if e.tick % 60 == 0 then
+            local player = game.players[1]
 
-    local close = settings.global["close-range"].value
-    local middle = settings.global["middle-range"].value
-    local distant = settings.global["distant-range"].value
+            local close = settings.global["close-range"].value
+            local middle = settings.global["middle-range"].value
+            local distant = settings.global["distant-range"].value
 
-    if withinRadius(player, close) then
-      local volume = player.mod_settings["close-volume"].value
-      player.play_sound{path = "geiger-close", volume_modifier = volume}
-    elseif withinRadius(player, middle) then
-      local volume = player.mod_settings["middle-volume"].value
-      player.play_sound{path = "geiger-middle", volume_modifier = volume}
-    elseif withinRadius(player, distant) then
-      local volume = player.mod_settings["close-volume"].value
-      player.play_sound{path = "geiger-distant", volume_modifier = volume}
+            if withinRadius(player, close) then
+                local volume = player.mod_settings["close-volume"].value
+                player.play_sound {path = "geiger-close", volume_modifier = volume}
+            elseif withinRadius(player, middle) then
+                local volume = player.mod_settings["middle-volume"].value
+                player.play_sound {path = "geiger-middle", volume_modifier = volume}
+            elseif withinRadius(player, distant) then
+                local volume = player.mod_settings["close-volume"].value
+                player.play_sound {path = "geiger-distant", volume_modifier = volume}
+            end
+        end
     end
-  end
-end
 )
-
